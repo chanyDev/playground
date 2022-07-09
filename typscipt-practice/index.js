86b38fb47ab6f8aede6a45a4cfa47d0c49aacb96
chanyDev <chanyd.dev@gmail.com>
2022-07-09 13:24:42 +0900

    사용하지 않는 프로젝트 제거

--- typscipt-practice/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const test = () => {
-    console.log('1');
-};
-test();
-const a = [1, 2, 3];
-const b = [...a, 4];
-new Promise(() => { });
--- typscipt-practice/test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.HelloWorld = void 0;
-const jsx_runtime_1 = require("react/jsx-runtime");
-const HelloWorld = () => (0, jsx_runtime_1.jsx)("h1", { children: "Hello world" });
-exports.HelloWorld = HelloWorld;
